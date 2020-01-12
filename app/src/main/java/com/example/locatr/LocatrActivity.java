93178a632a69aaeac10d9aa8a4affe5acf82f5c6
Alexander Slatinin <sandroisu1411@gmail.com>
2020-01-12 20:42:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/locatr/LocatrActivity.java
@@ -0,0 +1,14 @@
+package com.example.locatr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocatrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+}
